f93993fcf21ff7fcb1b3663d3da4b60324911c26
bl4ckd4y <bl4ckd4yfm@gmail.com>
2024-08-18 06:58:31 +0400

    nextLVL

---
+++ src/func.h
@@ -0,0 +1,13 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+double sqrt_newton(const double number);
+void sqrt_function_printf(const double number);
+
+double square(const double number);
+void square_function_printf(const double number);
+
+double sum(const double number);
+void sum_function_printf(const double number);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
 
+#include "func.h"
+
 int input(double* number);
-
-double sqrt_newton(const double number);
-void sqrt_function_printf(const double number);
-
-double square(const double number);
-void square_function_printf(const double number);
-
-double sum(const double number);
-void sum_function_printf(const double number);
 
 int main(void) {
     double number = 0;
@@ -32,42 +25,4 @@ int input(double* number) {
     } else
         flag = 1;
     return flag;
-}
-
-void sqrt_function_printf(const double number) {
-    if (number < 0) {
-        printf("---> Квадратный корень из отрицательного числа не определен,.\n");
-    } else {
-        double result = sqrt_newton(number);
-        printf("---> Квадратный корень из %.5lf равен %.5lf\n", number, result);
-    }
-}
-
-double sqrt_newton(const double number) {
-    double guess = number / 2.0;
-    double epsilon = 0.00001;
-
-    while ((guess * guess - number) > epsilon || (number - guess * guess) > epsilon) {
-        guess = (guess + number / guess) / 2.0;
-    }
-
-    return guess;
-}
-
-double square(const double number) {
-    double square = number * number;
-    return square;
-}
-
-void square_function_printf(const double number) {
-    printf("---> Квадрат числа %.5lf равен %.5lf\n", number, square(number));
-}
-
-double sum(const double number) {
-    double sum = number + number;
-    return sum;
-}
-
-void sum_function_printf(const double number) {
-    printf("---> Сумма числа %.5lf и числа %.5lf равна %.5lf\n", number, number, sum(number));
 }
