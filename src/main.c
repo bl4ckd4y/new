4b5c49bb9b9fe221a8a505314c8a88a05c9fa2de
bl4ckd4y <bl4ckd4yfm@gmail.com>
2024-08-18 05:48:30 +0400

    if,.

--- src/main.c
+++ src/main.c
@@ -1,20 +1,36 @@
 #include <stdio.h>
 
 double sqrt_newton(double number);
+int input(double* number);
+void sqrt_function(const double number);
 
 int main(void) {
-    double number;
+    double number = 0;
+    if (input(&number) == 0) {
+        sqrt_function(number);
+    } else {
+        printf("Вы ввели не число,. - завершение программы.\n");
+    }
+    return 0;
+}
 
+int input(double* number) {
+    int flag = 0;
     printf("Введите число: ");
-    scanf("%lf", &number);
+    if (scanf("%lf", number) == 1)
+        flag = 0;
+    else
+        flag = 1;
+    return flag;
+}
 
+void sqrt_function(const double number) {
     if (number < 0) {
-        printf("Квадратный корень из отрицательного числа не определен.\n");
+        printf("Квадратный корень из отрицательного числа не определен,.\n");
     } else {
         double result = sqrt_newton(number);
         printf("Квадратный корень из %.5lf равен %.5lf\n", number, result);
     }
-    return 0;
 }
 
 double sqrt_newton(double number) {
